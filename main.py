87bdf8796b85e6b4cef9c0ba92d15f820b109cf5
Laderlappenn <ciphra5@mail.ru>
2024-05-26 22:00:12 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,111 @@
+import tkinter as tk
+# from tkinter import ttk
+from tkinter import filedialog, messagebox
+from tkinter.messagebox import *
+import csv
+
+from pandas import Index
+import pandas
+import pandas as pd
+import json
+
+import ctypes, threading
+
+from utils import get_encoding_type, get_all_headers, unwrap_nester_hierarchy, check_nested_hierarchy, unwrap_nester_hierarchy_in_row
+#
+# from utils import get_encoding_type, get_ddl_type, get_engine_type
+# from updater import update
+
+
+class JsonParserApp:
+    def __init__(self, master):
+        self.filetypes = [("Json files", "*.json"), ]
+
+        self.master = master
+        self.master.title("Json Parser")
+
+        self.label = tk.Label(master, text="Выбор Json файла")
+        self.label.pack(padx=(25, 25), pady=(10, 10))
+
+        self.select_button = tk.Button(master, text="Select File", command=self.select_file)
+        self.select_button.pack(padx=(10, 10), pady=(10, 10))
+
+        self.generate_button = tk.Button(master, text="Generate CSV file from JSON", command=self.parse_json)
+        self.generate_button.pack(padx=(35, 35), pady=(10, 25))
+
+    def select_file(self):
+        self.filepath = filedialog.askopenfilename(filetypes=self.filetypes)
+        self.label.config(text="Выбранный файл: " + self.filepath)
+
+    def parse_json(self):
+        try:
+            if self.filepath:
+                if self.filepath.endswith('.json'):
+                    # try:
+                    #     #df = pandas.read_json(self.filepath, lines=True)
+                    #     data = []
+                    #     with open(self.filepath) as f:
+                    #         for line in f:
+                    #             data.append(json.loads(line))
+
+                        # for i in range(len(df.columns)):
+                        #     print(df[f"{df.columns[i]}"])
+                    # except UnicodeDecodeError as unicode_error:
+                    encoding = get_encoding_type(self.filepath)
+                    data = []
+                    with open(self.filepath, encoding=encoding) as f:
+                        for line in f:
+                            data.append(json.loads(line))
+
+                    # for dict_row in data:
+                    #     if check_nested_hierarchy(dict_row) == True:
+                    #         unwrap_nester_hierarchy_in_row(data)
+
+                    unwrap_nester_hierarchy(data)
+
+                    headers = get_all_headers(data)
+                    print(headers)
+                    # for i in range(len(data)):
+                    #     data[i] = str(data[i])
+
+                    # with open("csv_file.csv", 'w', newline='', encoding=encoding) as csvfile:
+                    #     writer = csv.DictWriter(csvfile, fieldnames=headers)
+                    #     writer.writeheader()
+                    #     for row in data:
+                    #         writer.writerow(row)
+                        #writer.writerows(data)
+
+                    with open("csv_file.csv", 'w', newline='', encoding=encoding) as csvfile:
+                        writer = csv.writer(csvfile)
+                        writer.writerow(headers)
+                        for row in data:
+                            print(row)
+                            writer.writerow([[row.get(key, None) for key in headers]])        #rowdict.get(key, self.restval) for key in self.fieldnames          #writer.writerow([list(row.values())]) #[value for value in row.values()]
+
+                    #df = pandas.read_json(self.filepath, lines=True, encoding=get_encoding_type(self.filepath))
+                else:
+                    messagebox.showerror("Error", "Unsupported file format")
+                    return
+
+            else:
+                messagebox.showerror("Error", "No file selected")
+        except Exception as e:
+            messagebox.showerror("Error", str(e) + " " + str(e.__class__.__name__))
+            print("Error", str(e) + " " + str(e.__class__.__name__))
+
+
+
+def main():
+    root = tk.Tk()
+    app = JsonParserApp(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    # success, message = update()
+    # if success:
+    #     print(message)
+    # else:
+    #     print("Failed:", message)
+    main()
+    
---
+++ utils.py
@@ -0,0 +1,55 @@
+from typing import List, Dict, Any, Set
+
+
+from chardet import detect
+from flatten_json import flatten
+
+
+def unwrap_nester_hierarchy_in_row(dict_row) -> None:
+    dict_row = flatten(dict_row)
+
+
+def unwrap_nester_hierarchy(data) -> None:
+    for i in range(len(data)):
+        data[i] = flatten(data[i])
+        #for dict_row in data:
+        #dict_row = flatten(dict_row)
+        #print(flatten(dict_row))
+
+
+def check_nested_hierarchy(dict_row):
+    return any(isinstance(dict_row, dict) for dict_row in dict_row.values())
+
+
+def get_all_headers(data: List[Dict[str, Any]]) -> List[str]:
+    # headers_counter = 0
+    # current_most_counted_headers_in_row = 0
+    # for i, data_row in enumerate(data):
+    #     if len(data_row.keys()) > headers_counter:
+    #         headers_counter = len(data_row.keys())
+    #         current_most_counted_headers_in_row = i
+    #
+    # headers = []
+    # for key in data[current_most_counted_headers_in_row].keys():
+    #     headers.append(key)
+    #
+    # #return headers
+    #
+    # dict_of_all_headers = dict.fromkeys(headers)
+    # # set_of_all_headers = set(headers)
+    # for data_row in data:
+    #     for cell, data in enumerate(data_row):
+    #         dict_of_all_headers.update({data: ""})
+    # return list(dict_of_all_headers)
+
+    dict_of_all_headers = dict()
+    for data_row in data:
+        for _, data in enumerate(data_row):
+            dict_of_all_headers.update({data: ""})
+    return list(dict_of_all_headers)
+
+
+def get_encoding_type(file):
+    with open(file, 'rb') as f:
+        rawdata = f.read(1024)
+    return detect(rawdata)['encoding']
